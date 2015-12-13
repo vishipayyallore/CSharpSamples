efedeb1199c3560dcea104e739974614c2436c09
vishipayyallore <vishipayyallore@yahoo.com>
2015-12-13 20:42:09 +0530

    Added CSharp.Samples.MongoStore project

---
+++ CSharp.Samples.MongoStore/MongoDataStore.cs
@@ -0,0 +1,35 @@
+﻿using CSharp.Samples.Utilities;
+using MongoDB.Driver;
+
+namespace CSharp.Samples.MongoStore
+{
+
+    public class MongoDataStore
+    {
+        #region Private Variables.
+        private static readonly MongoClient DataStoreClient;
+        private static readonly IMongoDatabase DataStoreDatabase;
+        #endregion
+
+        private MongoDataStore() { }
+
+        static MongoDataStore()
+        {
+            var mongoDbConnectionString = AppSettingsHelper.GetAppSettingsValue("MongoDbConnectionString");
+            DataStoreClient = new MongoClient(mongoDbConnectionString);
+
+            //Data Base
+            var mongoDbDatabaseName = AppSettingsHelper.GetAppSettingsValue("MongoDbDatabaseName");
+            DataStoreDatabase = DataStoreClient.GetDatabase(mongoDbDatabaseName);
+        }
+
+        #region Properties
+        public static MongoDataStore DataStoreInstance { get; } = new MongoDataStore();
+
+        public MongoClient MongoDataStoreClient => DataStoreClient;
+
+        public IMongoDatabase DatabaseInstance => DataStoreDatabase;
+        #endregion
+    }
+
+}
