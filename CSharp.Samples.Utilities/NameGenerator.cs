0c346dda18a15e2ee1ee7aa13094099335f8816e
vishipayyallore <vishipayyallore@yahoo.com>
2015-12-13 19:43:47 +0530

    Name Generator Code is added

---
+++ CSharp.Samples.Utilities/NameGenerator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+
+namespace CSharp.Samples.Utilities
+{
+
+    public class NameGenerator
+    {
+        #region Private variables.
+        static readonly string[] Consonants = { "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "l", "n", "p", "q", "r", "s", "sh", "zh", "t", "v", "w", "x" };
+        static readonly string[] Vowels = { "a", "e", "i", "o", "u", "ae", "y" };
+        static readonly Random RandomObject = new Random();
+        static StringBuilder _name;
+        #endregion
+
+        #region Methods.
+        public static string GenerateName(int length = 8)
+        {
+            _name = new StringBuilder(100);
+            length /= 2;
+            GetNamePart(length);
+            AddSpaceInName();
+            GetNamePart(length);
+
+            return _name.ToString();
+        }
+        #endregion
+
+        #region Private Methods.
+        private static string GetConsonant(bool isUpperCase = false)
+        {
+            var consonantsLength = Consonants.Length;
+            return isUpperCase ? Consonants[RandomObject.Next(consonantsLength)].ToUpper() : Consonants[RandomObject.Next(consonantsLength)];
+        }
+
+        private static string GetVowel()
+        {
+            var vowelsLength = Vowels.Length;
+            return Vowels[RandomObject.Next(vowelsLength)];
+        }
+
+        private static void AddSpaceInName()
+        {
+            _name.Append(" ");
+        }
+
+        private static void GetFirstPart()
+        {
+            _name.Append(GetConsonant(true));
+            _name.Append(GetVowel());
+        }
+
+        private static void GetRemainingName(int length)
+        {
+            var iCtr = 0;
+            while (iCtr <= length)
+            {
+                _name.Append(GetConsonant());
+                _name.Append(GetVowel());
+                iCtr += 2;
+            }
+        }
+
+        private static void GetNamePart(int length)
+        {
+            GetFirstPart();
+            GetRemainingName(length-2);
+        }
+        #endregion
+    }
+
+}
---
+++ CSharp.Samples.UtilitiesTests/NameGeneratorTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CSharp.Samples.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharp.Samples.UtilitiesTests
+{
+
+    [TestClass]
+    public class NameGeneratorTests
+    {
+
+        [TestMethod]
+        public void WhenASingleNameIsGenerated()
+        {
+            Console.WriteLine(NameGenerator.GenerateName());
+        }
+
+        [TestMethod]
+        public void When10NamesAreGenerated()
+        {
+            for (var iCtr = 1; iCtr <= 10; iCtr++)
+            {
+                Console.WriteLine(NameGenerator.GenerateName());
+            }
+        }
+    }
+
+}
