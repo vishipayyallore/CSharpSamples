8803768583c6910de91969a43fc088141f93fa65
vishipayyallore <vishipayyallore@yahoo.com>
2015-12-13 19:29:48 +0530

    Currency Generator code and unit tests written

---
+++ CSharp.Samples.Utilities/CurrencyGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CSharp.Samples.Utilities
+{
+
+    public class CurrencyGenerator
+    {
+        #region Variables
+        static readonly Random RandomObject = new Random();
+        private readonly double _minimumValue;
+        private readonly double _deltaValue;
+        #endregion
+
+        public CurrencyGenerator(double minimumValue = 1.0f, double deltaValue = 1000.0f)
+        {
+            _minimumValue = minimumValue;
+            _deltaValue = deltaValue;
+        }
+
+        #region Methods
+        public decimal GenerateAmount()
+        {
+            var randomNumber = RandomObject.NextDouble();
+            return (decimal) (_minimumValue + (randomNumber * _deltaValue));
+        }
+        #endregion
+    }
+
+}
---
+++ CSharp.Samples.UtilitiesTests/CurrencyGeneratorTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using CSharp.Samples.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharp.Samples.UtilitiesTests
+{
+
+    [TestClass]
+    public class CurrencyGeneratorTests
+    {
+
+        [TestMethod]
+        public void WhenACurrencyIsGenerated()
+        {
+            var currencyGenerator = new CurrencyGenerator();
+            Console.WriteLine(currencyGenerator.GenerateAmount());
+        }
+
+        [TestMethod]
+        public void When10DifferentCurrenciesAreGenerated()
+        {
+            var currencyGenerator = new CurrencyGenerator();
+            for (var iCtr = 1; iCtr <= 10; iCtr++)
+            {
+                Console.WriteLine(currencyGenerator.GenerateAmount());
+            }
+        }
+
+    }
+}
