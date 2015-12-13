3ad9a7afb2a3523c9e393c6ced619528d5bfe574
vishipayyallore <vishipayyallore@yahoo.com>
2015-12-13 19:54:43 +0530

    AppSettings Helper Added

---
+++ CSharp.Samples.Utilities/AppSettingsHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Configuration;
+
+namespace CSharp.Samples.Utilities
+{
+
+    public class AppSettingsHelper
+    {
+        #region Methdos.
+        public static string GetAppSettingsValue(string keyName)
+        {
+            return ConfigurationManager.AppSettings[keyName] ?? string.Empty;
+        }
+        #endregion
+    }
+
+}
---
+++ CSharp.Samples.UtilitiesTests/AppSettingsHelperTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CSharp.Samples.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharp.Samples.UtilitiesTests
+{
+
+    [TestClass]
+    public class AppSettingsHelperTests
+    {
+
+        [TestMethod]
+        public void WhenAppSettingsValueExists()
+        {
+            Console.WriteLine(AppSettingsHelper.GetAppSettingsValue("MongoDbConfiguration"));
+        }
+    }
+}
