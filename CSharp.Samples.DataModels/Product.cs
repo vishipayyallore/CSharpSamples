29f88289aeaaa80aa5e7e700b2caf4d190fe4ce4
vishipayyallore <vishipayyallore@yahoo.com>
2015-12-14 20:45:50 +0530

    Product Data Model Added

--- CSharp.Samples.DataModels/Product.cs
+++ CSharp.Samples.DataModels/Product.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CSharp.Samples.IDataModels;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace CSharp.Samples.DataModels
 {
-    public class Product
+
+    public class Product : IEntity
     {
+        /// <summary>
+        /// Default constructor needed for MongoDb
+        /// </summary>
+        public Product() { }
+
+        public Product(string name, decimal price, string manufacturer)
+        {
+            Name = name;
+            Price = price;
+            Manufacturer = manufacturer;
+        }
+
+        #region Properties
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Manufacturer { get; set; }
+        #endregion
     }
+
 }
